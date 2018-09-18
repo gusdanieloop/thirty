3508e3db67bdfc91737dff270364edee15e6938c
MATEUS JOSE MILCZEWSKI ALVES FARIAS <mateus.farias@jv01.local>
2018-09-17 21:06:33 -0300

    Improvements on menu display

--- thirty-web/src/pages/company/company-menu/company-menu.ts
+++ thirty-web/src/pages/company/company-menu/company-menu.ts
@@ -8,10 +8,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CompanyMenuPage {
 
+  dishes: boolean;
+  drinks: boolean;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.dishes = false;
+    this.drinks = false;
   }
 
   ionViewDidLoad() {
+  }
+
+  showMenuItem(itemType: string) {
+
+    if (itemType === 'dishes') {
+      this.dishes = !this.dishes;
+    }
+
+    if (itemType === 'drinks') {
+      this.drinks = !this.drinks;
+    }
+
   }
 
 }
