1dc6cb9ca66830cabce89961001cf137c506bf51
MATEUS JOSE MILCZEWSKI ALVES FARIAS <mateus.farias@jv01.local>
2018-09-21 21:04:14 -0300

    Implemented fake content on filter page

--- thirty-web/src/pages/filter/filter.ts
+++ thirty-web/src/pages/filter/filter.ts
@@ -8,6 +8,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FilterPage {
 
+  averageSpend;
+  companyType;
+  rating;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
