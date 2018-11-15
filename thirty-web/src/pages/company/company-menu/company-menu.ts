cffc5a959c9955a7dc65de89ee45310892a811bf
Mateus Jose Milczewski Alves Farias <mateus.farias@jv01.local>
2018-11-14 22:04:07 -0200

    Implemented menu request [frontend]

--- thirty-web/src/pages/company/company-menu/company-menu.ts
+++ thirty-web/src/pages/company/company-menu/company-menu.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { ServeHttpServiceProvider } from './../../../providers/serve-http-service/serve-http-service';
+
 import { Company } from './../../timeline/company.object';
+import { Menu } from './company-menu.object';
 
 @IonicPage()
 @Component({
@@ -11,17 +14,34 @@ import { Company } from './../../timeline/company.object';
 export class CompanyMenuPage {
 
   company: Company;
-
+  menuList: Menu[];
   dishes: boolean;
   drinks: boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private _uriPathDao: string = '/companies/companyMenu';
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private _serverHttp: ServeHttpServiceProvider,) {
     this.company = this.navParams.get('selectedCompany');
     this.dishes = false;
     this.drinks = false;
   }
 
   ionViewDidLoad() {
+    this.getMenus();
+  }
+
+  getMenus() {
+    this._serverHttp.httpReadbyId(this._uriPathDao + '/', this.company.id).subscribe(
+      (list: Menu[]) => {
+        list.length > 0 ? this.menuList = list : this.menuList = undefined;
+        console.log('menuList', this.menuList);
+
+      },
+        error => alert('Ocorreu um erro.')
+    );
   }
 
   showMenuItem(itemType: string) {
